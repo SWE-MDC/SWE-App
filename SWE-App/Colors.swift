bb8bccd1a8a312e81cac43ccb00f33e13f15654e
Sam <sammyweig@gmail.com>
2023-10-09 17:20:08 -0400

    Make fake login functional

---
+++ SWE-App/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  SWE
+//
+//  Created by Sam Weigman on 10/1/23.
+//
+
+import SwiftUI
+
+extension Color{
+    public static let customPurple = Color(UIColor(red: 94/255.0, green: 81/255.0, blue: 123/255.0, alpha: 2))
+}
--- SWE-App/ContentView.swift
+++ SWE-App/ContentView.swift
@@ -1,21 +1,86 @@
 //
 //  ContentView.swift
-//  SWE-App
+//  SWE
 //
-//  Created by Sam Weigman on 10/9/23.
+//  Created by Sam Weigman on 10/1/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack{
+
+                Color.customPurple.ignoresSafeArea()
+                Circle()
+                    .offset(y: 120)
+                    .scale(1.2)
+                    .foregroundColor(.white)
+                
+               
+                VStack {
+                    Image("Logo")
+                        .offset(y: -140)
+                        .padding()
+                    
+
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.customPurple)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                    
+                }
+            }
+        } .navigationBarHidden(true)
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        //change later for our own database
+        //use "usertest" as the fake username and "passtest" as the fake password
+        if username.lowercased() == "usertest" {
+            wrongUsername = 0
+            if password.lowercased() == "passtest" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
         }
-        .padding()
     }
 }
 
