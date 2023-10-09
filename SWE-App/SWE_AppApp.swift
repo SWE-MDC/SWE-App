5e1861ef7b6a243f6a5afa8e4e44856c0e8e0423
Sam <sammyweig@gmail.com>
2023-10-09 12:13:06 -0400

    initial commit

---
+++ SWE-App/SWE_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWE_AppApp.swift
+//  SWE-App
+//
+//  Created by Sam Weigman on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SWE_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
