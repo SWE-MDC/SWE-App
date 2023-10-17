07b811cc5188272d72c58bf5b3292c34db7c7fd1
Sam <sammyweig@gmail.com>
2023-10-17 16:29:33 -0400

    Add signup page

--- SWE-App/SignUpView.swift
+++ SWE-App/SignUpView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct SignUpView: View {
+    
+    //not suitable for rotation or scaling
+    @State private var width = UIScreen.main.bounds.width
+    @State private var height = UIScreen.main.bounds.height
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+
+
+
+
+    
+    
     var body: some View {
-        Text("sign up")
+        NavigationView {
+            ZStack {
+                Rectangle()
+                    .fill(Color.customPurple)
+                    .frame(width: width, height: 370, alignment: .top)
+                    .ignoresSafeArea()
+                    .position(x: width/2, y: 0)
+                
+                VStack {
+                    Image("Logo")
+                        .position(x: width/2, y: 70)
+                    
+                    Text("Enter Your Email")
+                    
+                    TextField("Email", text: $email)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                    //add logic to check password requirements
+                    Text("Enter a Password")
+
+                    TextField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                    //add in logic for confirming password
+                    Text("Confirm Your Password")
+                
+                    TextField("Confirm Password", text: $confirmPassword)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                    
+                }
+            }
+        }
     }
 }
 
 #Preview {
     SignUpView()
 }
+
