62ec2066db946793064a07699ae44aebc6d37544
Sam <sammyweig@gmail.com>
2023-10-19 11:16:52 -0400

    create button for event login

--- SWE-App/HomeScreenView.swift
+++ SWE-App/HomeScreenView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 //NOT for admin use
+//view future events? link to google calendar
+//check into current event
+//settings/view profile
 
 struct HomeScreenView: View {
-    //view future events? link to google calendar
-    //check into current event
-    //settings/view profile
+    
+    @State private var width = UIScreen.main.bounds.width
+    @State private var height = UIScreen.main.bounds.height
+    
+
     var body: some View {
-        Text("Hello, there user!")
+        NavigationView {
+            VStack {
+                Button("Event Login") {
+                    //take to event login
+                }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 200)
+                .background(Color.customPurple)
+                .cornerRadius(30)
+            }
+
+        }
     }
 }
 
