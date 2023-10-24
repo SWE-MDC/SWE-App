9b35975ad5f9a1d2e0e5b607761c8fd3cb935177
Sam <sammyweig@gmail.com>
2023-10-24 10:23:12 -0400

    fix homescreen

--- SWE-App/HomeScreenView.swift
+++ SWE-App/HomeScreenView.swift
@@ -11,80 +11,6 @@ import SwiftUI
 //view future events? link to google calendar
 //check into current event
 //settings/view profile
-struct MenuItem: Identifiable {
-    var id = UUID()
-    var text = String()
-    let handler: () -> Void = {
-        print("tapped item")
-    }
-}
-
-struct MenuContent: View {
-    let items: [MenuItem] = [
-        MenuItem(text: "Home"),
-        MenuItem(text: "Settings"),
-        MenuItem(text: "Profile"),
-        MenuItem(text: "Log Out")
-
-    ]
-    var body: some View {
-        ZStack{
-            Color(Color.white)
-            
-            VStack(alignment: .leading, spacing: 0) {
-                ForEach(items) { item in
-                    HStack{
-                        Text(item.text)
-                            .bold()
-                            .foregroundColor(Color.customPurple)
-                            .font(.system(size: 22))
-                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
-                        
-                        Spacer()
-                    }
-                    .padding()
-                    
-                }
-                Spacer()
-            }
-            .padding(.top, 50)
-        }
-        
-    }
-    
-}
-
-
-struct SideMenu: View {
-    let width: CGFloat
-    let menuOpened: Bool
-    let toggleMenu: () -> Void
-    var body: some View {
-        ZStack{
-            GeometryReader{ _ in
-                EmptyView()
-            }
-            .background(Color.gray.opacity(0.5))
-            .opacity(self.menuOpened ? 1 : 0)
-            .animation(Animation.easeIn.delay(0.25))
-            .onTapGesture{
-                self.toggleMenu()
-            }
-        }
-        
-        HStack {
-            MenuContent()
-                .frame(width: width)
-                .offset(x: menuOpened ? 0 : -width)
-                .animation(.default)
-            Spacer()
-        }
-        
-    }
-    
-}
-
-
 
 struct HomeScreenView: View {
     
@@ -95,22 +21,6 @@ struct HomeScreenView: View {
     var body: some View {
         ZStack{
             Color.customPurple
-            if !menuOpened {
-                Button(action : {
-                    //openMenu
-                    self.menuOpened.toggle()
-                }, label: {
-                    Image("Settings")
-//                    Text("Open Menu")
-//                        .foregroundColor(Color.customPurple)
-//                        .frame(width: 50, height: 50)
-//                        .background(Color(Color.white))
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                        .position(x: 50, y: 90)
-                })
-                
-            }
             
             
             VStack{
@@ -156,8 +66,8 @@ struct HomeScreenView: View {
                     .padding(0.4)
 
                     
-                    Button("Extra idk") {
-                        //take to somewhere
+                    Button("Settings") {
+                        //take to settings
                     }
                     .font(.system(size: 20, design: .rounded)
                         .weight(.bold))
@@ -171,15 +81,10 @@ struct HomeScreenView: View {
 
             }
 
-            SideMenu(width: width/1.6, menuOpened: menuOpened, toggleMenu: toggleMenu)
 
         } .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
-    
-    
-    func toggleMenu() {
-        menuOpened.toggle()
-    }
+
 }
 
 #Preview {
