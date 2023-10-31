d330d7bb5391d08a26d8f0f63ea8233944bf6a5a
OSU2018 <97775100+OSU2018@users.noreply.github.com>
2023-10-31 12:24:57 -0400

    Formated the date and time entry as well as update the buttons on the bottom.

--- SWE-App/AdminView.swift
+++ SWE-App/AdminView.swift
@@ -9,31 +9,48 @@ import SwiftUI
 
 struct AdminView: View {
     @State private var eventName = ""
-    @State private var eventDate = ""
+//    @State private var eventDate = ""
     @State private var eventDetails = ""
     @State private var eventLocation = ""
+    @State private var date = Date()
+    @State var currentTime = Date()
 
 
+    var closedRange = Calendar.current.date(byAdding: .year, value: -1, to: Date())!
     
     
     var body: some View {
         NavigationView{
             VStack() {
                 Text("ADMIN VIEW")
+                    .padding()
                 
-                TextField("Event Name", text: $eventName)
+                TextField("Event Name/Title", text: $eventName)
                     .padding()
                     .frame(width: 300, height: 50)
                     .background(Color.black.opacity(0.05))
                     .cornerRadius(10)
                     .textInputAutocapitalization(.never)
                 
-                TextField("Event Date", text: $eventDate)
-                    .padding()
-                    .frame(width: 300, height: 50)
-                    .background(Color.black.opacity(0.05))
-                    .cornerRadius(10)
-                    .textInputAutocapitalization(.never)
+//                TextField("Event Date", text: $eventDate)
+//                    .padding()
+//                    .frame(width: 300, height: 50)
+//                    .background(Color.black.opacity(0.05))
+//                    .cornerRadius(10)
+//                    .textInputAutocapitalization(.never)
+                
+                // https://www.youtube.com/watch?v=9UovPNh4Csw
+                Section(){
+                    DatePicker("Date & Time: ", selection: $currentTime, in: Date()...)
+                    //DatePicker("Date & Time: ", selection: $currentTime, in: Date()...).labelsHidden() use this if you don't want the label to show.
+                }
+                .padding()
+                .frame(width: 300, height: 80)
+                .background(Color.black.opacity(0.05))
+                .cornerRadius(10)
+
+
+                
                 
                 TextField("Event Location", text: $eventLocation)
                     .padding()
@@ -53,8 +70,19 @@ struct AdminView: View {
                     //creates a new event in database
                     //need to update event name in homescreenview
                 }
+                .foregroundColor(.white)
+                .frame(width: 300, height: 50)
+                .background(Color.customPurple)
+                .cornerRadius(10)
+                
+                NavigationLink(destination: LoginView()) { Text("Back to Login")}
+                    .frame(width: 300, height: 50)
+                
+//                Section(header:Text("Result")){ //you can remove this
+//                    Text("\(currentTime)")
+//                }
             }
-        }
+        }.navigationBarHidden(true)
     }
 }
 
