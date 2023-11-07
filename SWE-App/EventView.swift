468f4ed3d51a8112e7e119d0f0d657bf477134c8
Sam <sammyweig@gmail.com>
2023-11-07 11:43:32 -0500

    add menu to eventView

--- SWE-App/EventView.swift
+++ SWE-App/EventView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct EventView: View {
+    @State private var showMenu: Bool = false
+    @State var menuOpened = false;
+    @State private var width = UIScreen.main.bounds.width
+    @State private var height = UIScreen.main.bounds.height
+    
+    
     var body: some View {
         NavigationView {
-            List(events){ event in
-                NavigationLink(destination: DetailEventView(event: event)){
-                    ExtractedView(events:event)
+            ZStack {
+                List(events){ event in
+                    NavigationLink(destination: DetailEventView(event: event)){
+                        ExtractedView(events:event)
+                    }
+                }
+                
+                
+                
+                
+                GeometryReader { _ in
+                    HStack {
+                        Spacer()
+                        sideMenu()
+                        //hides the side menu with showMenu is false
+                        .offset(x: showMenu ? 0 : width)
+                        .animation(.easeInOut(duration: 0.3), value: showMenu)
+                    }
+                }
+                //when side menu is open, it darkens the rest of the screen
+                .background(Color.black.opacity(showMenu ? 0.5 : 0))
+            }
+            
+            .toolbar{
+                
+                Button{
+                    //allows user to toggle the menu side bar
+                    self.showMenu.toggle()
+                } label: {
+                    //if side menu is showing, display an "x" icon
+                    if showMenu {
+                        Image(systemName: "xmark")
+                            .font(.title)
+                            .foregroundColor(.black)
+                    } else { //side menu not showing, show 3 bars
+                        Image(systemName: "text.justify")
+                            .font(.title)
+                            .foregroundColor(Color.customYellow)
+                    }
                 }
             }
         }
